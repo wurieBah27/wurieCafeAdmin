c40f2bf842cc3d088708da2e5de41cbf5c3bd4c9
wurieBah27 <wuriebah27@gmail.com>
2025-04-11 14:31:34 +0400

    fixing th price issue

--- src/features/products/SingleProductDetails.jsx
+++ src/features/products/SingleProductDetails.jsx
@@ -63,7 +63,8 @@ const SingleProductDetails = () => {
                   <div className="flex items-end gap-3 text-2xl font-extrabold text-gray-900 dark:text-white sm:text-3xl">
                     <div>
                       <p className="text-sm">
-                        Price: AED <span>{discountPrice?.toFixed(2)}</span>
+                        Price: AED{" "}
+                        <span>{(price - discountPrice)?.toFixed(2)}</span>
                       </p>
                     </div>
                     <div>
